7cf6e53d3bbbe47bfbd3c80ad9350e61460c6dac
ferattass555 <ferattas2763@gmail.com>
2024-05-25 10:10:05 +0300

    Öklid Mesafesi Hesaplama 2. Hafta

--- Hafta-2/oklid-mesafe-hesaplama.py
+++ Hafta-2/oklid-mesafe-hesaplama.py
@@ -1,22 +1,22 @@
-# Noktaların tanımlanması
+#Noktaları tanımlıyorum
 points = [(2, 3), (5, 7), (1, 1), (8, 2), (4, 4)]
 
-# Öklid mesafesi hesaplayan fonksiyon
+# Öklid mesafesi hesaplayan fonksiyonun denklemi: d = √(x₂-x₁)²+(y₂-y₁)²
 def euclideanDistance(point1, point2):
     return ((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2) ** 0.5
 
-# Mesafelerin hesaplanması
+# Mesafelerin hesapla
 distances = []
 for i in range(len(points)):
     for j in range(i + 1, len(points)):
         dist = euclideanDistance(points[i], points[j])
         distances.append(dist)
 
-# Minimum mesafenin bulunması
+# Minimum mesafeyi bul
 min_distance = distances[0]
 for distance in distances:
     if distance < min_distance:
         min_distance = distance
 
-# Sonucun yazdırılması
+# Sonucun yazdır
 print(f"Minimum Öklid mesafesi: {min_distance}")
